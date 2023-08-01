539adb03d105b76a3330a522170e28cb84ce9662
Alejandro Piñango <alejandropinango@gmail.com>
2023-08-01 19:13:31 -0400

    Data base records added by db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "Creating magic characters!!"
+require 'faker'
+
+20.times do
+  Character.create(
+    name: Faker::Movies::HarryPotter.unique.character,
+    location: Faker::Movies::HarryPotter.location,
+    house: Faker::Movies::HarryPotter.house
+  )
+end
